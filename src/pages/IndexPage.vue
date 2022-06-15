dc71436ce6d15940b91c7cfd6d96c5b9d9cdd667
afonsoburginski <burginskikevin@gmail.com>
2022-06-15 09:40:58 -0400

    Consumindo dados

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,121 +1,129 @@
 <template>
-  <q-page class="flex flex-center q-px-lg q-pb-md">
+  <div class="q-pa-lg">
 
-    <q-timeline color="secondary">
-      <q-timeline-entry heading text="Timeline heading" />
+    <q-timeline :layout="layout" :side="side" color="secondary">
+      <q-timeline-entry heading>Timeline</q-timeline-entry>
 
+    <!-- passo1 -->
+    <ul>
+      <li v-for="passo1 in passo1" key="passo1">
+        {{passo1}}
+        <q-timeline-entry
+          :title="title"
+          :subtitle="subtitle"
+          :color="color"
+          :icon="icon"
+          :body="body"
+        >
+        </q-timeline-entry>
+      </li>
+    </ul>
+
+    <!-- passo2 -->
       <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        avatar="https://cdn.quasar.dev/img/avatar3.jpg"
-        :text="text"
-      />
+        :title="title"
+        :subtitle="subtitle"
+        :color="color"
+        :icon="icon"
+        :body="body"
+      >
+      </q-timeline-entry>
 
+    <!-- passo3 -->
       <q-timeline-entry
-        title="Event Title"
-        subtitle="February 21, 1986"
-        icon="delete"
-        :text="text"
-      />
+        :title="title"
+        :subtitle="subtitle"
+        :color="color"
+        :icon="icon"
+        :body="body"
+      >
+      </q-timeline-entry>
 
-      <q-timeline-entry heading text="November, 2017" />
-
+    <!-- passo4 -->
       <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        :text="text"
-      />
+        :title="title"
+        :subtitle="subtitle"
+        :color="color"
+        :icon="icon"
+        :body="body"
+      >
+      </q-timeline-entry>
 
+    <!-- passo5 -->
       <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        :text="text"
-      />
+        :title="title"
+        :subtitle="subtitle"
+        :color="color"
+        :icon="icon"
+        :body="body"
+      >
+      </q-timeline-entry>
 
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        color="orange"
-        icon="done_all"
-        :text="text"
-      />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        :text="text"
-      />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        :text="text"
-      />
     </q-timeline>
-
-  </q-page>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+export default {
+  setup () {
+    return {
 
-export default defineComponent({
-  name: "IndexPage",
-});
+      passo1: {
+        title:"Cotação de preço Aprovada",
+        subtitle:"13/06/2022",
+        color:"green",
+        icon:"price_check",
+        body: 'Cotação aberta por Mario Frias e Aprovada por João Martins de Oliveira as 10:02 do dia 13 de Junho de 2022'
+      },
 
-// --- array de passos
+      passo2: {
+        title:"Nota Fiscal Emitida",
+        subtitle:"14/06/2022",
+        color:"green",
+        icon:"request_quote",
+        text: "Fornecedor Cirurgica Al-Styn emitir nota fiscal numero 12312321",
+        fotos: [
+          { text: 'Nota Fiscal', src: 'https://assets.b9.com.br/wp-content/uploads/2018/02/Google-Imagens.png' }
+        ],
+        links: [
+          { text: 'Nota Fiscal', href: 'www.google.com.br' }
+        ]
+      },
 
-[
-  {
-    title:"Cotação de preço Aprovada",
-    subtitle:"13/06/2022",
-    color:"green",
-    icon:"price_check",
-    text: "Cotação aberta por Mario Frias e Aprovada por João Martins de Oliveira as 10:02 do dia 13 de Junho de 2022"
-  },
-  {
-    title:"Nota Fiscal Emitida",
-    subtitle:"14/06/2022",
-    color:"green",
-    icon:"request_quote",
-    text: "Fornecedor Cirurgica Al-Styn emitir nota fiscal numero 12312321",
-    fotos: [
-      { text: 'Nota Fiscal', src: 'https://assets.b9.com.br/wp-content/uploads/2018/02/Google-Imagens.png' }
-    ],
-    links: [
-      { text: 'Nota Fiscal', href: 'www.google.com.br' }
-    ]
-  },
-  {
-    title:"Produto enviado",
-    subtitle:"15/06/2022",
-    color:"green",
-    icon:"request_quote",
-    text: "Fornecedor Cirurgica Al-Styn Enviou seu produto e esta disponivel para rastreamento no link abaixo",
-    links: [
-      { text: 'Rastreio', href: 'www.google.com.br' }
-    ]
-  },
-  {
-    title:"Produto entregue",
-    subtitle:"25/06/2022",
-    color:"green",
-    icon:"request_quote",
-    text: "Fornecedor informou que o produto já foi entregue",
-    fotos: [
-      { text: 'Canhoto da nota fiscal', src: 'https://assets.b9.com.br/wp-content/uploads/2018/02/Google-Imagens.png' }
-    ],
-    links: [
-      { text: 'Canhoto da nota fiscal', href: 'www.google.com.br' }
-    ]
-  },
-  {
-    title:"Aguardando Conferencia do Cliente",
-    subtitle:"--/--/----",
-    color:"grey",
-    icon:"request_quote",
-    text: ""
+      passo3: {
+        title: "Produto enviado",
+        subtitle: "15/06/2022",
+        color: "green",
+        icon: "request_quote",
+        text: "Fornecedor Cirurgica Al-Styn Enviou seu produto e esta disponivel para rastreamento no link abaixo",
+        links: [
+          { text: 'Rastreio', href: 'www.google.com.br' }
+        ]
+      },
+
+      passo4: {
+        title:"Produto entregue",
+        subtitle:"25/06/2022",
+        color:"green",
+        icon:"request_quote",
+        text: "Fornecedor informou que o produto já foi entregue",
+        fotos: [
+          { text: 'Canhoto da nota fiscal', src: 'https://assets.b9.com.br/wp-content/uploads/2018/02/Google-Imagens.png' }
+        ],
+        links: [
+          { text: 'Canhoto da nota fiscal', href: 'www.google.com.br' }
+        ]
+      },
+
+      passo5: {
+        title:"Aguardando Conferencia do Cliente",
+        subtitle:"--/--/----",
+        color:"grey",
+        icon:"request_quote",
+        text: ""
+      }
+
+    }
   }
-]
-
+}
 </script>
