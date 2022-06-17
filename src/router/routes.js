4b4d0cf210ca162a518aa722eb056780e8c02331
marjo <marcosaraujo.tele@gmail.com>
2022-06-17 16:43:34 -0400

    Co-authored-by: Afonso Kevin Burginski <afonsoburginski@users.noreply.github.com>

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,16 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    path: "/timeline/:id",
+    component: () => import("layouts/TimeLine.vue"),
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/forumariotal",
+    component: () => import("layouts/TimeLine.vue"),
+  },
+  {
+    path: "/componentenao sei o que",
+    component: () => import("layouts/TimeLine.vue"),
+  },
+];
 
-export default routes
+export default routes;
